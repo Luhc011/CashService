973fcf6ab0c39cb22b97c78622abb7b4c7d78bc9
Lucas H <lucashcardoso26@gmail.com>
2023-09-29 00:33:15 -0300

    Add project files.

---
+++ MoneyExtensionTests/MoneyExtensionTests.cs
@@ -0,0 +1,15 @@
+﻿using MoneyExtension;
+
+namespace MoneyExtensionTests
+{
+    public class MoneyExtensionTests
+    {
+        [Fact]
+        public void ShouldConvertDecimalToInt()
+        {
+            decimal valor = 112.23m;
+            var cents = valor.ToCents();
+            Assert.Equal(11223, cents);
+        }
+    }
+}
